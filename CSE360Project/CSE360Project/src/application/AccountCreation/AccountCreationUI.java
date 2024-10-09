ae078df62dbae77f13f20fab9cbcee1bdab495a1
Kathan Parag Shah <158689114+kathanparagshah@users.noreply.github.com>
2024-10-08 20:15:39 -0700

    InitialRegistrationUI.java

--- CSE360Project/CSE360Project/src/application/AccountCreation/AccountCreationUI.java
+++ CSE360Project/CSE360Project/src/application/AccountCreation/AccountCreationUI.java
@@ -6,7 +6,74 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.stage.Stage;package application.AccountCreation;
+
+import application.Core.Navigation;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+
+public class InitialRegistrationUI {
+
+    public static void RegisterWithNavigation() {
+        Label emailLabel = new Label("Email:");
+        Label usernameLabel = new Label("Username:");
+        Label passwordLabel = new Label("Password:");
+        Label phoneLabel = new Label("Phone Number:");
+        Label roleLabel = new Label("Role:");
+
+        TextField emailField = new TextField();
+        emailField.setPromptText("Enter your email");
+
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("Choose a username");
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Choose a password");
+
+        TextField phoneField = new TextField();
+        phoneField.setPromptText("Enter your phone number");
+
+        ComboBox<String> roleComboBox = new ComboBox<>();
+        roleComboBox.getItems().addAll("User", "Admin", "Other");
+        roleComboBox.setValue("User");
+
+        Button submitButton = new Button("Create Account");
+        submitButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
+        submitButton.setOnAction(event -> {
+            String email = emailField.getText();
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+            String phone = phoneField.getText();
+            String role = roleComboBox.getValue();
+            Navigation.navigate("FinishRegistrationPage");
+        });
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setVgap(10);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(20));
+
+        grid.add(emailLabel, 0, 0);
+        grid.add(emailField, 1, 0);
+        grid.add(usernameLabel, 0, 1);
+        grid.add(usernameField, 1, 1);
+        grid.add(passwordLabel, 0, 2);
+        grid.add(passwordField, 1, 2);
+        grid.add(phoneLabel, 0, 3);
+        grid.add(phoneField, 1, 3);
+        grid.add(roleLabel, 0, 4);
+        grid.add(roleComboBox, 1, 4);
+        grid.add(submitButton, 1, 5);
+
+        Scene scene = new Scene(grid, 400, 350);
+        Navigation.registerScene("InitialRegistrationPage", scene);
+    }
+}
+
 
 public class AccountCreationUI {
 
